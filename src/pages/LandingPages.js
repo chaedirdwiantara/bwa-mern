844ba1999e8930122bb5fa752a58259306675e35
Chaedir Dwiantara <chaedir.dwiantara@sinbad.co.id>
2022-06-21 10:15:58 +0700

    initialcommit

---
+++ src/pages/LandingPages.js
@@ -0,0 +1,16 @@
+import { Text, StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+
+import second from "elements/Button";
+
+export default class LandingPages extends Component {
+  render() {
+    return (
+      <View>
+        <Text>LandingPageyos</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
